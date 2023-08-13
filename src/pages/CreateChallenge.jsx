2bf9814eb46737c105885d9391044a211a424723
nargis21 <nargisakther1933@gmail.com>
2023-08-13 16:54:08 +0600

    add new challnge part design done

--- src/pages/CreateChallenge.jsx
+++ src/pages/CreateChallenge.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react"
-import { SandpackProvider, 
-  SandpackLayout, 
-  SandpackCodeEditor, 
+import { useState, useEffect } from "react";
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
   SandpackPreview,
   SandpackFileExplorer,
   SandpackTests,
@@ -10,9 +11,9 @@ import { SandpackProvider,
   useSandpack,
   useActiveCode,
   SandpackStack,
-  FileTabs
-} from "@codesandbox/sandpack-react"
-import { Editor } from "@monaco-editor/react"
+  FileTabs,
+} from "@codesandbox/sandpack-react";
+import { Editor } from "@monaco-editor/react";
 import { getFileLanguage } from "../utils/fileHelper";
 
 function MonacoEditor({ setFiles }) {
@@ -47,8 +48,8 @@ function MonacoEditor({ setFiles }) {
   );
 }
 
-function CodeEditorWrapper({ setFiles }){
-  const {code, updateCode} = useActiveCode()
+function CodeEditorWrapper({ setFiles }) {
+  const { code, updateCode } = useActiveCode();
   const { sandpack } = useSandpack();
   const activeFile = sandpack.activeFile;
 
@@ -60,18 +61,18 @@ function CodeEditorWrapper({ setFiles }){
     }));
   }
   return (
-    <SandpackCodeEditor 
-          showTabs
-          showLineNumbers={true}
-          showInlineErrors
-          wrapContent
-          closableTabs
-          onChange={handleUpdateCode}
-          />
-  )
+    <SandpackCodeEditor
+      showTabs
+      showLineNumbers={true}
+      showInlineErrors
+      wrapContent
+      closableTabs
+      onChange={handleUpdateCode}
+    />
+  );
 
   // return (
-  //   <Editor 
+  //   <Editor
   //    height="100%"
   //    defaultLanguage="javascript"
   //    defaultValue={code}
@@ -83,186 +84,206 @@ function CodeEditorWrapper({ setFiles }){
   // )
 }
 
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import { defaultFilesWithTests, defaultMarkdown } from "../config/challenge";
 import { client } from "../api/api-client";
 
-
-export default function CreateChallenge(){
-
+export default function CreateChallenge() {
   const [files, setFiles] = useState(() => defaultFilesWithTests);
-  const [markdown, setMardown] = useState(() => (defaultMarkdown))
-  const [fileName, setFileName] = useState("")
+  const [markdown, setMardown] = useState(() => defaultMarkdown);
+  const [fileName, setFileName] = useState("");
   const [token, setToken] = useState(() =>
     window.localStorage.getItem("accessToken")
   );
-  console.log('files : ',files)
+  console.log("files : ", files);
 
-  function handleAddFile(){
-    const newFileName = fileName
-    setFiles(currentFiles => {
-       if(currentFiles.newFileName){
-        return currentFiles
-       }else{
-        return {...currentFiles, [newFileName]: ``}
-       }
-    })
+  function handleAddFile() {
+    const newFileName = fileName;
+    setFiles((currentFiles) => {
+      if (currentFiles.newFileName) {
+        return currentFiles;
+      } else {
+        return { ...currentFiles, [newFileName]: `` };
+      }
+    });
   }
 
-  async function handleFormSubmit(event){
-    event.preventDefault()
-    const formBody = {}
-    const formData = new FormData(event.currentTarget)
-    formData.forEach((value, property) => formBody[property] = value);
+  async function handleFormSubmit(event) {
+    event.preventDefault();
+    const formBody = {};
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((value, property) => (formBody[property] = value));
 
     const reqBody = {
       title: formBody.title,
       challengeCategory: formBody.challengeCategory,
       difficultyLevel: formBody.difficultyLevel,
       description: markdown,
-      files: JSON.stringify(files)
-    }
-    console.log('reqBody : ', reqBody)
+      files: JSON.stringify(files),
+    };
+    console.log("reqBody : ", reqBody);
 
     // send req to backend
-    await client('challenges', {data: reqBody, token})
+    await client("challenges", { data: reqBody, token });
   }
 
   return (
-    <>
-    <div>
-      Create Challenge
-    </div>
-
-    <form onSubmit={handleFormSubmit}>
-    <div className="w-full h-[500px]">
-      <div className="max-w-lg mx-auto">
-        <div className="w-full flex flex-col justify-center align-center">
-
-          <div className="form-control w-full">
-          <label className="label">
-            <span className="label-text">Challenge Title?</span>
-          </label>
-          <input name="title" type="text" placeholder="Type here" className="input input-bordered w-full" />
-          {/* <label className="label">
-            <span className="label-text-alt">Bottom Left label</span>
-            <span className="label-text-alt">Bottom Right label</span>
-          </label> */}
-          </div>
-
-          <div className="form-control w-full">
-          <label className="label">
-            <span className="label-text">Challenge Category</span>
-          </label>
-          <select name="challengeCategory" className="select select-bordered">
-            <option disabled selected>Pick one</option>
-            <option>UI</option>
-            <option>Custom Hooks</option>
-            <option>User Events</option>
-            <option>Pattern</option>
-          </select>
-          {/* <label className="label">
-            <span className="label-text-alt">Alt label</span>
-            <span className="label-text-alt">Alt label</span>
-          </label> */}
-          </div>
-
-          <div className="form-control w-full">
-          <label className="label">
-            <span className="label-text">Difficulty Level</span>
-          </label>
-          <select name="difficultyLevel" className="select select-bordered">
-            <option disabled selected>Pick one</option>
-            <option>Easy</option>
-            <option>Medium</option>
-            <option>Hard</option>
-          </select>
-          {/* <label className="label">
-            <span className="label-text-alt">Alt label</span>
-            <span className="label-text-alt">Alt label</span>
-          </label> */}
-          </div>
-          </div>
-      </div>
-
-      <div className="w-full mx-auto my-6">
-        <div>Description</div>
-        <div className="flex h-[500px] p-2">
-          <div className="w-[50%] border-dashed border-2 border-sky-500 overflow-y-scroll">
-           <ReactMarkdown className="markdown" rehypePlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
-          </div>
-          <div className="w-[50%] border-dashed border-2 border-sky-500">
-          <Editor
-            height="465px"
-            width="100%"
-            language="markdown"
-            theme="vs-dark"
-            defaultValue={markdown}
-            onChange={(value) => setMardown(value)}
-          />
-          </div>
-        </div>
-      </div>
-
-      <div>
-      <div className="w-full mx-auto my-6">
-        <div>File Object</div>
-        <SandpackProvider template="react" theme="dark" files={files}
-      options={{ 
-        visibleFiles: [""],
-        activeFile: "/App.js",
-        readOnly: true,
-      }}
-      >
-
-        <div className="flex my-4 p-2">
-          <div className="w-[100%] py-4 border-dashed border-2 border-sky-500">
-            <div className="max-w-lg mx-auto">
-              <div className="w-full flex flex-col justify-center align-center">
-              <div className="form-control w-full">
+    <div className="flex justify-center items-center bg-green-100 py-10">
+      <div className="lg:w-[60%] md:w-[80%] w-[95%] shadow-xl p-6 rounded bg-green-200">
+        <h1 className="text-2xl py-6 text-center font-semibold text-gray-600">
+          Add New Challenge
+        </h1>
+        <form onSubmit={handleFormSubmit}>
+          <div className="w-full flex flex-col justify-center align-center">
+            <div className="form-control w-full mb-2">
               <label className="label">
-                <span className="label-text">Add File</span>
+                <span className="label-text font-semibold text-gray-600 font-semibold text-gray-600">
+                  Challenge Title
+                </span>
               </label>
-              <input type="text" value={fileName} onChange={(e) => setFileName(e.target.value)} placeholder="e.g : /filename.ext" className="input text-slate-500 input-bordered w-full" />
+              <input
+                name="title"
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered focus:outline-none focus:ring-2 focus:ring-emerald-400  w-full"
+              />
               {/* <label className="label">
-                <span className="label-text-alt">Bottom Left label</span>
-                <span className="label-text-alt">Bottom Right label</span>
-              </label> */}
-              <div className="py-4">
-               <button className="btn btn-sm btn-success" onClick={handleAddFile}>Add</button>
-              </div>
-              
-              </div>
-              </div>
+            <span className="label-text font-semibold text-gray-600-alt">Bottom Left label</span>
+            <span className="label-text font-semibold text-gray-600-alt">Bottom Right label</span>
+          </label> */}
+            </div>
+            <div className="form-control w-full mb-2">
+              <label className="label">
+                <span className="label-text font-semibold text-gray-600">
+                  Challenge Category
+                </span>
+              </label>
+              <select
+                name="challengeCategory"
+                className="select select-bordered focus:outline-none focus:ring-2 focus:ring-emerald-400 "
+              >
+                <option disabled selected>
+                  Pick one
+                </option>
+                <option>UI</option>
+                <option>Custom Hooks</option>
+                <option>User Events</option>
+                <option>Pattern</option>
+              </select>
+              {/* <label className="label">
+            <span className="label-text font-semibold text-gray-600-alt">Alt label</span>
+            <span className="label-text font-semibold text-gray-600-alt">Alt label</span>
+          </label> */}
+            </div>
+            <div className="form-control w-full mb-2">
+              <label className="label">
+                <span className="label-text font-semibold text-gray-600">
+                  Difficulty Level
+                </span>
+              </label>
+              <select
+                name="difficultyLevel"
+                className="select select-bordered focus:outline-none focus:ring-2 focus:ring-emerald-400 "
+              >
+                <option disabled selected>
+                  Pick one
+                </option>
+                <option>Easy</option>
+                <option>Medium</option>
+                <option>Hard</option>
+              </select>
+              {/* <label className="label">
+            <span className="label-text font-semibold text-gray-600-alt">Alt label</span>
+            <span className="label-text font-semibold text-gray-600-alt">Alt label</span>
+          </label> */}
             </div>
           </div>
-        </div>
 
-        <div className="my-4 py-4 h-[500px]">
-        <SandpackLayout className="h-full">     
-          <SandpackFileExplorer />
-          {/* <MonacoEditor setFiles={setFiles} /> */}
-          <CodeEditorWrapper setFiles={setFiles} />
-          <SandpackPreview 
-            showNavigator={true} 
-            showOpenInCodeSandbox={false}
-          />
-          {/* <SandpackTests /> */}
-          {/* <SandpackConsole /> */}
-        </SandpackLayout>
-        </div>
+          <div className="w-full mb-2">
+            <label className="label">
+              <span className="label-text font-semibold text-gray-600">
+                Description
+              </span>
+            </label>
 
-      </SandpackProvider>
+            <div className=" mb-3">
+              <Editor
+                height="465px"
+                width="100%"
+                language="markdown"
+                theme="vs-dark"
+                defaultValue={markdown}
+                onChange={(value) => setMardown(value)}
+              />
+            </div>
+            <div className="border-2 border-emerald-400 overflow-y-scroll px-4 bg-base-100">
+              <ReactMarkdown className="markdown" rehypePlugins={[remarkGfm]}>
+                {markdown}
+              </ReactMarkdown>
+            </div>
+          </div>
+
+          <div className="w-full mb-2">
+            <label className="label">
+              <span className="label-text font-semibold text-gray-600">
+                File Object
+              </span>
+            </label>
+            <SandpackProvider
+              template="react"
+              theme="dark"
+              files={files}
+              options={{
+                visibleFiles: [""],
+                activeFile: "/App.js",
+                readOnly: true,
+              }}
+            >
+              <div className="form-control mb-3">
+                <div className=" input-group">
+                  <input
+                    type="text"
+                    value={fileName}
+                    onChange={(e) => setFileName(e.target.value)}
+                    placeholder="e.g : /filename.ext"
+                    className="input input-bordered w-full focus:outline-none focus:ring-2 focus:ring-emerald-400 text-slate-800"
+                  />
+                  <button
+                    className="btn bg-gradient-to-r from-emerald-300 to-green-300 hover:from-emerald-400 hover:to-green-400 border-none"
+                    onClick={handleAddFile}
+                  >
+                    Add
+                  </button>
+                </div>
+              </div>
+
+              <div className="">
+                <SandpackLayout className="h-full">
+                  <SandpackFileExplorer />
+                  {/* <MonacoEditor setFiles={setFiles} /> */}
+                  <CodeEditorWrapper setFiles={setFiles} />
+                  {/* <SandpackPreview
+                      showNavigator={true}
+                      showOpenInCodeSandbox={false}
+                    /> */}
+                  {/* <SandpackTests /> */}
+                  {/* <SandpackConsole /> */}
+                </SandpackLayout>
+              </div>
+            </SandpackProvider>
+          </div>
+
+          <div className="flex justify-center items-center mt-6 w-full">
+            <button
+              type="submit"
+              className="btn bg-gradient-to-r from-emerald-300 to-green-300 hover:from-emerald-400 hover:to-green-400 border-none w-full"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-      </div>
-
-      <div className="flex justify-center items-center">
-        <button type="submit" className="btn btn-success">Submit</button>
-      </div>
-
     </div>
-    </form>
-    </>
-  )
+  );
 }
